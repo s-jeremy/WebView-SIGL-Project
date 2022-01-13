549a31658bbadb76baadadc409d6bd3cfb3670d0
s-jeremy <jeremy.selo@gmail.com>
2022-01-13 13:19:05 +0100

    Introduce to WebView on the web site

--- app/src/main/java/com/example/webviewsiglproject/MainActivity.kt
+++ app/src/main/java/com/example/webviewsiglproject/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.webviewsiglproject
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat.startActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.loadUrl("https://intheclub.fr/")
+        myWebView.webViewClient = CallBack()
+        setContentView(myWebView)
+        myWebView.settings.javaScriptEnabled = true
+
+    }
+
+    private class CallBack : WebViewClient() {
+
+        override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
+            return false
+        }
     }
 }
